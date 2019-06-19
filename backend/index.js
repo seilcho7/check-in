e51056225560b066f6f146f303519da995f7a33c
Seil Cho <seilcho7@hotmail.com>
2019-06-19 12:44:27 -0400

    adds geolocation

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const app = express();
+const port = 3001;
+const cors = require('cors');
 
+app.use(cors());
+
+app.get('/api/customers', (req, res) => {
+    const customers = [
+        {id: 1, name: `Seil`},
+        {id: 2, name: 'Hong'}
+    ];
+    res.json(customers);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
