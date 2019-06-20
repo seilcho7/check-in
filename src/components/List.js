bbc5dc8847f742606971f128c452aed13adcdf8e
Seil Cho <seilcho7@hotmail.com>
2019-06-20 15:27:20 -0400

    adds map

--- src/components/List.js
+++ src/components/List.js
@@ -15,20 +15,24 @@ export default class List extends React.Component {
             data
         })
         console.log(this.state.data);
-        
     }
 
     render() {
+        
         return (
             <div>
                     {this.state.data.map((post, i) => {
                         const date = new Date(post.timestamp).toLocaleString();
+                        const url = `http://maps.google.com/maps?q=${post.location.lat}, ${post.location.lon}&z=15&output=embed`;
                     return (
                         <ul key={i}>
+                            <li>
+                                <div><iframe title="map" width="100%" height="600" src={url} frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"><a href="https://www.maps.ie/map-my-route/">Map a route</a></iframe></div>
+                            </li>
                             <li>Location: {post.location.lat.toFixed(2)}&deg;, {post.location.lon.toFixed(2)}&deg;</li>
                             <li>Mood: {post.mood}</li>
                             <li>{date}</li>
-                            <li><img src={post.picture}></img></li>
+                            <li><img src={post.picture} alt=""></img></li>
                         </ul>
                     )
                     })}
