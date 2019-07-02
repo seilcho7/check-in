fcda593e11b942b3323da40f20b470bd71cf822d
Seil Cho <seilcho7@hotmail.com>
2019-07-02 13:57:51 -0400

    adds build

---
+++ build/precache-manifest.119758a4142a70bddf4577710953ab03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384d2f6a57d5c77cd1d7326bed910ef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a9608b60007797dbfa9",
+    "url": "/static/css/main.faa21193.chunk.css"
+  },
+  {
+    "revision": "77df178414531feceee1",
+    "url": "/static/js/2.093e9e4f.chunk.js"
+  },
+  {
+    "revision": "3a9608b60007797dbfa9",
+    "url": "/static/js/main.0489470c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
